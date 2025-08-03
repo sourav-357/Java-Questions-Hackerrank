f9675e2b54285d3943c64826b500833fe6e4e7d5
Sourav Kumar <xsouravkumar357@gmail.com>
2025-08-03 18:23:28 +0530

    Day 2 completed

---
+++ Day_02/Loops.java
@@ -0,0 +1,18 @@
+package Day_02;
+
+import java.io.*;
+
+public class Loops {
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(bufferedReader.readLine().trim());
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(N + " x " + i + " = " + (N * i));
+        }
+
+        bufferedReader.close();
+    }
+}
---
+++ Day_02/Output.java
@@ -0,0 +1,20 @@
+package Day_02;
+
+import java.util.*;
+
+public class Output {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int i = scan.nextInt();
+        double d = scan.nextDouble();
+        scan.nextLine(); // Consume the leftover newline
+        String s = scan.nextLine();
+
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+
+        scan.close();
+    }
+}
